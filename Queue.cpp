ad63a707137656b180a7516035ad1aaa9e884839
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-05-28 10:37:38 +0700

    Menambahkan pengecekan queue underflow

--- Queue.cpp
+++ Queue.cpp
@@ -47,5 +47,14 @@ public:
     }
 
     void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+
+    }
 }
 
